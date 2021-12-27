761e3e4c751ba77227f6b2095f9b38473f8be0ea
luccorrea <lucas.correa@mercadolivre.com>
2021-12-27 17:28:34 -0300

    repository

---
+++ src/main/java/com/example/restaurante/repository/Main.java
@@ -0,0 +1,11 @@
+package com.example.restaurante.repository;
+
+import com.example.restaurante.entity.Mesa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    List<Mesa> m1 = new ArrayList<Mesa>();
+
+}
