733ed5c778a659754ee3ddd39e2cae65cde28667
Iberê Abondanza Kuhlmann <ibere.kuhlmann@mercadolivre.com>
2021-12-27 15:17:46 -0300

    implmentação do Service

---
+++ src/main/java/com/example/restaurante/entity/Caixa.java
@@ -0,0 +1,16 @@
+package com.example.restaurante.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Caixa {
+    private BigDecimal valorTotal;
+}
